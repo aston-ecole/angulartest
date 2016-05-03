86f223b4011058eb795976ef6b2c4bc0c5485622
root <root@localhost.localdomain>
2016-05-03 22:55:11 +0200

    premier commit

--- js/app.js
+++ js/app.js
@@ -1,22 +1,44 @@
 (function(){
 	var app = angular.module('store', []);
+	// controller Store
 	app.controller('StoreController', function(){
 	this.product = gem;
 	});
+	// controller panel
+	app.controller("PanelController", function(){
+		this.tab =1;
+		
+		this.selectTab = function(setTab) { // fonction selection tab
+		this.tab = setTab;
+		};
+		
+		this.isSelected = function(checkTab){ // fonction is selected
+		return this.tab === checkTab;
+		};
+	});
+	// data
 	var gem = [
 		{
 		name : 'Diamond',
 		price : 2.95,
 		description : "nothing",	
 		disponible : 1,
-		cache : false
+		cache : false,
+		image : [{
+			full: 'img/diamond.jpg',
+			thumb: 'img/diamond.jpg'
+		}]
 		},
 		{
 		name : 'Diamond2	',
 		price : 6.90,
 		description : "WOUAH",	
 		disponible : 8,
-		cache : false
+		cache : false,
+		image : [{
+			full: 'img/diamond.jpg',
+			thumb: 'img/diamond.jpg'
+		}]
 		}
 	]
 })();
